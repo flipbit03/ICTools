c3127b1cc16332bf96c195cd5e09cc8b9cd90bae
Cadu <cadu.coelho@gmail.com>
2018-03-28 16:26:07 -0300

    Adicionada Ferramenta HmCadTrabMan

---
+++ HmCadTrabMan/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace HmCadTrabMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(848, 409);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(584, 26);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(602, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gerar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(683, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Copiar p/ Área de Transf.";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 463);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(886, 502);
+            this.Name = "Form1";
+            this.Text = "HmCadTrabMan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HmCadTrabMan/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using ControleProducaoDAOS;
+
+namespace HmCadTrabMan
+{
+    public partial class Form1 : Form
+    {
+        ControleProducaoDAO dao;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dao = new ControleProducaoDAO();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<PeriodoData> periodos = dao.ListaPeriodosApropriacao();
+
+            comboBox1.DataSource = periodos;
+            comboBox1.DisplayMember = "Repr1";
+        }
+
+
+        private String GeraTabelaComTotal(Dictionary<string, decimal> _dados, decimal _total, string _title = "HM TOTAL")
+        {
+            StringBuilder r = new StringBuilder();
+
+            r.AppendFormat("  {0,20}                {1,10}{2}{2}", "EQUIPAMENTO", _title, Environment.NewLine);
+
+            foreach(KeyValuePair<string, decimal> kp in _dados)
+            {
+                r.AppendFormat("  {0,20}                {1,10}{2}", kp.Key, kp.Value.ToString("0,0.00"), Environment.NewLine);
+            }
+            r.AppendFormat("{0}", Environment.NewLine);
+
+            r.AppendFormat("  {0,20}                {1,10}{2}{2}", "", "---------------", Environment.NewLine);
+
+            r.AppendFormat("  {0,20}                {1,10}{2}", "", "TOTAL = "+_total.ToString("0,0.00"), Environment.NewLine);
+
+            return r.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PeriodoData x = (PeriodoData)comboBox1.SelectedItem;
+
+            HmCadTrabManData dados = dao.HmCadTrabMan(x.idPeriodo.GetValueOrDefault());
+
+            StringBuilder relat = new StringBuilder();
+
+            relat.Append(String.Format(
+                "         GERENCIA DE CONTROLE - ICC{0}" +
+                "HORAS MAQUINAS TRABALHADAS POR ATIVIDADES{0}{0}", Environment.NewLine));
+
+            relat.AppendFormat("Periodo: {0}{1}{1}", x.Repr1, Environment.NewLine);
+
+            // Montar HMCADTRAB
+            relat.AppendFormat("HMCADTRAB - HORAS-MAQUINA TRABALHADAS{0}{0}", Environment.NewLine);
+            relat.Append(GeraTabelaComTotal(dados.hmcadtrab, dados.sum_hmcadtrab));
+
+            // Montar HMCADMAN
+            relat.AppendFormat("{0}{0}HMCADMAN - HORAS-MAQUINA MANUTENCAO{0}{0}", Environment.NewLine);
+            relat.Append(GeraTabelaComTotal(dados.hmcadman, dados.sum_hmcadman, "HM MANUTENCAO"));
+
+
+            textBox1.Text = relat.ToString();
+
+            button2.Text = "Copiar p/ Área de Transf.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+            button2.Text = "Copiado!";
+        }
+
+    }
+}
