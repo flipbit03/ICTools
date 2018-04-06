ec231ebe880023de275d1fb123c810ab7bd2c335
Cadu <cadu.coelho@gmail.com>
2018-04-06 09:18:15 -0300

    Adicionado ícone ao projeto HmCadTrabMan

--- HmCadTrabMan/Form1.Designer.cs
+++ HmCadTrabMan/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -91,6 +92,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(886, 502);
             this.Name = "Form1";
             this.Text = "HmCadTrabMan";
